27c16b7b2a240cccdb09d7e290b4dc00cce8cae0
Eliasvanroosbroeck <91074359+Eliasvanroosbroeck@users.noreply.github.com>
2024-01-02 20:23:06 +0100

    Delete frontend directory

--- frontend/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer : React.FC = () => {
-    return(
-        <footer>
-            <div className="container text-center">
-                <p>Copyright ©&nbsp;</p>
-            </div>
-        </footer>
-    )
-}
